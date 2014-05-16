bd296be516b2d2286458a77464565e7c4a09793b
colorfulfool <colorfulfool@gmail.com>
2014-05-16 16:21:58 +0300

    Added dummy implementation of password reset

--- galleria/galleria/urls.py
+++ galleria/galleria/urls.py
@@ -8,8 +8,13 @@ urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
     # url(r'^login$', LoginView.as_view(), name='login'),
     # url(r'^logout$', LogOut.as_view(), name='logout'),
-    url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    
+    url(r'^password-reset/$', 'django.contrib.auth.views.password_reset', {'template_name': 'password_reset.html'}),
+    url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'password_reset.html'}, name='password_reset_done'),
+    url(r'^password-reset/confirm/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'password_reset.html'}, name='password_reset_done'),
+    url(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'password_reset.html'}, name='password_reset_done'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
