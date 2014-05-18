8899e4e07d431a97a6f76dd80846d71fdede4ba7
humphrey <hum3@drummond.info>
2014-05-18 11:57:10 +0100

    Scripting user creation with ansible, also correcting problem with paths and scripts so runs on main

---
+++ galleria/galleria/management/commands/add_superuser.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from optparse import make_option
+
+class Command(BaseCommand)
+    option_list = BaseCommand.option_list + (
+        make_option('--user',
+            action='store',
+            default=None,
+            help='Username for new user'),
+        make_option('--password',
+            action='store',
+            default=None,
+            help='User password'),
+        make_option('--email',
+            action='store',
+            default=None,
+            help='User email address'),
+        )
+
+    def handle(self, *args, **kwargs):
+        user = User.object.create_superuser(
+            username = kwargs.get('user'),
+            password = kwargs.get('password'),
+            email = kwargs.get('email')
+            )
+        user.save()
--- galleria/galleria/urls.py
+++ galleria/galleria/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
-from views import HomeView
+from .views import HomeView
 
 from django.contrib import admin
 admin.autodiscover()
