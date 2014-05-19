22a370d50ade8657a7b06b4caae94367390f7d96
humphrey <hum3@drummond.info>
2014-05-19 23:37:43 +0100

    Removing reference to __unicode__ in contacts/admin.py

--- galleria/contacts/admin.py
+++ galleria/contacts/admin.py
@@ -2,19 +2,20 @@ from django.contrib import admin
 from .models import ContactType, Contact, PhoneNumber, Address, Note
 
 class ContactTypeAdmin(admin.ModelAdmin):
-	list_display = ('__unicode__', 'name')
-	list_editable = ('name',)
+    list_display = ('__str__', 'name')
+    list_editable = ('name',)
 
 class NoteInline(admin.TabularInline):
-	model = Note
+    model = Note
 
 class AddressInline(admin.StackedInline):
-	model = Address
+    model = Address
 
 class PhoneNumberInline(admin.TabularInline):
 	model = PhoneNumber
 
 class ContactAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'name_first', 'name_last')
 	inlines = [NoteInline, AddressInline, PhoneNumberInline]
 
 admin.site.register(ContactType, ContactTypeAdmin)
