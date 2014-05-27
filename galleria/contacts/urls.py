92aaeda75545b1e7efce2ff96393c25a3da4cc11
drummonds <hum3@drummond.info>
2014-05-27 10:44:27 +0100

    Fixed URL bug with contacts and done a draft contact_detail view

--- galleria/contacts/urls.py
+++ galleria/contacts/urls.py
@@ -13,19 +13,19 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^', ContactList.as_view(), name='contact_list'),
-    url(r'^contacts$', ContactView.as_view(), name='contact'),
-    url(r'^new/$', ContactCreate.as_view(), name='contact_create'),
-    url(r'^(?P<pk>\d+)/$', ContactDetail.as_view(), name='contact_detail'),
-    url(r'^(?P<pk>\d+)/update/$', ContactUpdate.as_view(), name='contact_update'),
-    url(r'^(?P<pk>\d+)/delete/$', ContactDelete.as_view(), name='contact_delete'),
-
     url(r'^type/', ContactTypeList.as_view(), name='contacttype_list'),
     url(r'contacttypess$', ContactTypeView.as_view(), name='contact_type'),
     url(r'^type/new/$', ContactTypeCreate.as_view(), name='contacttype_create'),
     url(r'^type/(?P<pk>\d+)/$', ContactTypeDetail.as_view(), name='contacttype_detail'),
     url(r'^type/(?P<pk>\d+)/update/$', ContactTypeUpdate.as_view(), name='contacttype_update'),
     url(r'^type/(?P<pk>\d+)/delete/$', ContactTypeDelete.as_view(), name='contacttype_delete'),
+
+    url(r'^contacts$', ContactView.as_view(), name='contact'),
+    url(r'^new/$', ContactCreate.as_view(), name='contact_create'),
+    url(r'^(?P<pk>\d+)/$', ContactDetail.as_view(), name='contact_detail'),
+    url(r'^(?P<pk>\d+)/update/$', ContactUpdate.as_view(), name='contact_update'),
+    url(r'^(?P<pk>\d+)/delete/$', ContactDelete.as_view(), name='contact_delete'),
+    url(r'^', ContactList.as_view(), name='contact_list'), # Must be last as catches all
 
 )
 
