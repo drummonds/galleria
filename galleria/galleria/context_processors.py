924235172a2011b226bdefda0374035c3152e227
humphrey <hum3@drummond.info>
2014-05-27 15:59:15 +0100

    Adding galleria_frontpage to context processors

--- galleria/galleria/context_processors.py
+++ galleria/galleria/context_processors.py
@@ -3,6 +3,7 @@ from django.conf import settings
 def galleria_context_processor(request):
     my_dict = {
         'galleria_brand': settings.GALLERIA_BRAND,
+        'galleria_frontpage': settings.GALLERIA_FRONTPAGE,
     }
 
     return my_dict
