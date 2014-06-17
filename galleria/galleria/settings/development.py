262865533c042a2d0aff0075c4ad7f74c7ea2d9d
drummonds <hum3@drummond.info>
2014-06-17 14:18:54 +0100

    Adding git checkout of galleria_private to deployment

--- galleria/galleria/settings/development.py
+++ galleria/galleria/settings/development.py
@@ -1,10 +1,6 @@
-"""
-Django settings for galleria development project.
 
 """
-"""
-Local Django settings for galleria project.
-
+Django settings for running galleria from a development site.
 """
 
 from .base import *
@@ -12,17 +8,51 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-INSTALLED_APPS += ("debug_toolbar", )
+THIRD_PARTY_APPS += (
+    'debug_toolbar',
+    )
+
+MIDDLEWARE_CLASSES = (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+  ) + MIDDLEWARE_CLASSES
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': "%s.true" % __name__,
+}
+
+def true(request):
+    return True
 
 #Allow local override which is per deployment instance.  There should probably then be an instance
 # git for version control of that data
+# Note this is trying to read from galleria_private on the newly deployed box
 try:
     import sys
     private_path = BASE_PRIVATE_DIR.child('development')
     sys.path.append(private_path)
     from private_settings import *
 except ImportError:
-    print(" No development overide private_settings.py found in  = {}".format(private_path))
+    print(" No development override private_settings.py found in  = {}".format(private_path))
     # If it doesnt' exist that is fine and just use application defaults
+
+
+#()()
+
+
+#Allow local override which is per deployment instance.  There should probably then be an instance
+# git for version control of that data
+try:
+    import sys
+    #Use production settings to be just like production in this release.  The private path will need to match
+    private_path = BASE_PRIVATE_DIR.child('staging')
+    sys.path.append(private_path)
+    from private_settings import *
+
+except ImportError:
+    print(" No staging override private_settings.py found in  = {}".format(private_path))
+    # If it doesnt' exist that is fine and just use application defaults
+
 
 
